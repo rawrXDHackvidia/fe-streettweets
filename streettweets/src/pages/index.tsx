424313b0910891935cf963e10babe5796d3f43e6
risalstr <risa.lestari2002@gmail.com>
2025-05-02 14:51:28 +0700

    initiate repo nextjs & tailwindcss project

---
+++ streettweets/src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Hello Tailwind + Next.js 15!
+      </h1>
+    </main>
+  );
+}
